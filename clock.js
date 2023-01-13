707f37a7efa6737e395c66299287e436eed5613f
Emerson Lima <emersonlimago123@gmail.com>
2023-01-12 22:26:51 -0300

    first version of project

---
+++ clock.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Jul 25, 2025 17:37:25").getTime();
+
+var x = setInterval(function(){
+    var now = new Date().getTime(); //gets current time
+
+    var distance = countDownDate - now; //calculates distance
+
+    //calculations
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("clock").innerHTML = days + " : " + hours + " : " + minutes + " :  " + seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("clock").innerHTML = "EXPIRED"
+    }
+}, 1000);
+
+x();
